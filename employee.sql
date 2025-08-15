f29044b85e623b6b7c889498d70d1b54277b1cba
Vishal Kumar <134846666+VishalSliet@users.noreply.github.com>
2025-08-15 11:52:47 +0530

    Create employee.sql

---
+++ employee.sql
@@ -0,0 +1,9 @@
+CREATE TABLE employee (
+    emp_id INT PRIMARY KEY,             -- Unique Employee ID
+    first_name VARCHAR(50) NOT NULL,    -- Employee's first name
+    last_name VARCHAR(50) NOT NULL,     -- Employee's last name
+    department VARCHAR(50),             -- Department name
+    hire_date DATE NOT NULL,            -- Date of joining
+    salary DECIMAL(10, 2),               -- Employee's salary
+    email VARCHAR(100) UNIQUE            -- Unique email
+);
